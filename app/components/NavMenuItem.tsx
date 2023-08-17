2c8a08ac320c3a00d61c149f17051e95a1aa857a
gabjuh <jjjjgabesz@gmail.com>
2023-08-17 08:53:52 +0200

    create components folder and move files into it

---
+++ app/components/NavMenuItem.tsx
@@ -0,0 +1,26 @@
+import INavMenuItem from "@/interfaces/INavMenuItem";
+import Link from "next/link";
+
+const NavMenuItem: React.FC<INavMenuItem> = ({
+  index,
+  title,
+  link,
+  selected,
+  handleClick,
+  isDropdown
+}) => {
+  return (
+    <li>
+      <Link
+        href={link}
+        className={`mx-4 whitespace-nowrap cursor-pointer px-1 py-3`}
+        onClick={() => handleClick(index)}
+      >
+        {title}
+      </Link>
+      {!isDropdown && <div className={`h-[4px] ${selected ? 'w-[95%]' : 'w-[0%]'}  hover:w-[95%] bg-secondary mx-auto mt-1 transition-all duration-200 ease-in-out`}></div>}
+    </li>
+  );
+};
+
+export default NavMenuItem;
