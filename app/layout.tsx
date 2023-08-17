03677b259d977dac5398d3aa95e931e4c4a5ba56
gabjuh <jjjjgabesz@gmail.com>
2023-08-17 12:27:20 +0200

    add fetch to rootlayout and add data to Nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,29 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
 
+  async function getData() {
+    const res = await fetch('https://api.franciskahajdu.de/data.json')
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    return res.json()
+  }
+
+  const data = await getData();
+
   return (
     <html lang="en" data-theme="dark">
       <body className={inter.className}>
-        <Nav />
-        <p>home asd</p>
+        <Nav data={[
+          data.menuItems,
+          data.settings
+        ]} />
+        <div className="mt-10">
+          <p>home asd</p>
+          <p>{data.timeStamp}</p>
+          <p>{data.impressum[0].sheetId}</p>
+        </div>
+        {children}
       </body>
     </html>
   )
