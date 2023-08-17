71911d0ee9019d143a9c6c399ae691d9ac92b4b5
gabjuh <jjjjgabesz@gmail.com>
2023-08-17 17:35:28 +0200

    fetch data, import Hero and Events components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import IData from "@/interfaces/IData";
+import Hero from "./components/Hero";
+import Events from "./components/Events";
+
+export default async function HomePage() {
+
+  async function getData() {
+    const res = await fetch('https://api.franciskahajdu.de/data.json')
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    return res.json()
+  }
+
+  const data: IData = await getData();
+
   return (
     <main className="">
-      asd
+      <Hero data={data} />
+      <Events data={data}/>
     </main>
   )
 }
