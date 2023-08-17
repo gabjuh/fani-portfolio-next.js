842e46bb9f1a736ead7002d3f0dc9f860f1d188c
gabjuh <jjjjgabesz@gmail.com>
2023-08-17 18:12:30 +0200

    create cv page and turn old ver. to ssr component

---
+++ app/cv/page.tsx
@@ -0,0 +1,51 @@
+import IData from '@/interfaces/IData';
+import PageContainer from '../components/PageContainer';
+import Title from '../components/Title';
+import ImageAndText from '../components/ImageAndText';
+import Link from 'next/link';
+
+export default async function CVPage() {
+
+  async function getData() {
+    const res = await fetch('https://api.franciskahajdu.de/data.json')
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    const data = res.json();
+    return data;
+  }
+
+  const data: IData = await getData();
+
+  const cv = data.cv
+
+  return (
+    <PageContainer>
+
+      <Title title={data ? cv[0].pageTitle : ''} />
+
+      {cv.map((item, index) => {
+        return (
+          <ImageAndText
+            key={index}
+            driveId={item.driveId}
+            alt={item.imgAlt}
+            imageLeft={item.imgOnSide?.toLowerCase() === 'left' ? true : false}
+            text={item.text}
+          />
+        );
+      })}
+
+      <p className="text-center mt-24">
+        <Link
+          className="btn btn-secondary text-white ml-4"
+          href="/#about"
+          // onClick={handleClick ? () => handleClick(1) : () => null}
+        >
+          {cv[0].buttonText}
+        </Link>
+      </p>
+
+    </PageContainer>
+  )
+}
