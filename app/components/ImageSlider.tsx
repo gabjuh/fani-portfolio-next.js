772fa2ff727e17d264d03736789aa9346fd0b18f
gabjuh <jjjjgabesz@gmail.com>
2023-08-18 06:11:24 +0200

    Create ImageSlider and sat as ssr component with the data prop

---
+++ app/components/ImageSlider.tsx
@@ -0,0 +1,57 @@
+// import SliderImage from '../SliderImage';
+import getElementWidth from '../../helpers/getElementWidth';
+import IData from '../../interfaces/IData';
+import Image from 'next/image';
+
+const ImageSlider = ({ data } : {data: IData}) => {
+
+  const slider = data.slider;
+
+  return (
+    <>
+      <div className="carousel" id="carousel">
+        {slider.map((item, index) => {
+          return (
+            <div
+              key={`slider-img-${index}`}
+              id={`carousel-item-${index}`}
+              className={`carousel-item w-full drop-shadow-md`}
+            >
+              <Image 
+                src={`https://api.franciskahajdu.de/img/${item.fileName}`}
+                alt={`Slider Image ${index}`}
+                height="600"
+                width="600"
+                style={{
+                  height: '500px',
+                  objectFit: 'contain'
+                }}
+              />  
+              {/* <SliderImage
+                driveId={item.driveId}
+                alt={item.imgAlt}
+                // className={``}
+                containerWidth={getElementWidth('#carousel')}
+                index={index}
+              /> */}
+
+            </div>
+          );
+        })
+        }
+      </div>
+
+      {/* Buttons */}
+      <div className="flex justify-center w-full py-10 gap-2">
+        {slider.map((_, index) => {
+          return (
+            <a key={`slider-button-${index}`} href={`#carousel-item-${index}`} className="btn btn-xs">{index + 1}</a>
+          );
+        }
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ImageSlider;
