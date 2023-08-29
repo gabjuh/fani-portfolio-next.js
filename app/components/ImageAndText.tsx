6a9e509369139f058c152364c6720485f8520e6b
gabjuh <jjjjgabesz@gmail.com>
2023-08-29 08:49:14 +0200

    change text-image position

--- app/components/ImageAndText.tsx
+++ app/components/ImageAndText.tsx
@@ -19,9 +19,9 @@ const ImageAndText: React.FC<IImageAndText> = ({
     <>
       <div className={`flex ${imageLeft ? 'flex-col' : 'flex-col-reverse'} md:flex-row lg:my-16 my-4`}>
         {imageLeft && <ImageForText fileName={fileName} driveId={driveId || ''} alt={alt || 'image'} classNameForImg={classNameForImg} />}
-        <div className={`w-full md:w-1/2 flex flex-col ${!imageLeft ? 'items-end' : ''} justify-center`}>
+        <div className={`w-full md:w-4/6 flex flex-col ${!imageLeft ? 'items-end' : ''} justify-center md:ml-10 mr-10`}>
           <div 
-            className={`${imageLeft ? textAlign === 'justify' ? 'md:text-justify' : 'md:text-left' : textAlign === 'justify' ? 'md:text-justify' : 'md:text-right'} md:ml-8 lg:ml-0 md:mr-8 lg:mr-0 text-center leading-8`}
+            className={`${imageLeft ? textAlign === 'justify' ? 'md:text-justify' : 'md:text-left' : textAlign === 'justify' ? 'md:text-justify' : 'md:text-right'} md:ml-10 lg:ml-0 lg:mr-0 text-center leading-8 px-8 md:p-0`}
           >
             <ReactMarkdown
               remarkPlugins={[remarkGfm]}
