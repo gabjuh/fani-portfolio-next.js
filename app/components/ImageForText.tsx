f3cae06be2eb31d0f9844c70dfb01eea38af3afe
gabjuh <jjjjgabesz@gmail.com>
2023-08-29 08:49:26 +0200

    change img width

--- app/components/ImageForText.tsx
+++ app/components/ImageForText.tsx
@@ -16,6 +16,10 @@ const ImageForText: React.FC<IImageForText> = ({
           width="400"
           height="200"
           alt={alt} 
+          style={{
+            width: '380px',
+            height: 'auto',
+          }}
           className={`${classNameForImg} mx-auto mb-6 md:mb-0`} 
         />
       </div>
