d46f97994d2ac20338220fb7ad54ba795e3bae7a
gabjuh <jjjjgabesz@gmail.com>
2023-09-04 08:37:21 +0200

    deactivate everything about color and language buttons WIP

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
 import React, { useState, useEffect, useContext, MouseEventHandler } from "react";
-import INav from "@/interfaces/INav"
+import INav from "@/interfaces/INav";
 import NavMenuItem from "./NavMenuItem";
 import TableIdContext from "@/providers/AppProvider";
 import Link from "next/link";
@@ -27,97 +27,97 @@ const Nav: React.FC<INav> = ({ data: [menu, settingsArr] }) => {
     scrollToId(link);
     console.log(link);
     setSelectedMenuItem(link);
-  }
+  };
 
   // Get the current system theme
-  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  const [theme, setTheme] = useState<'light' | 'dark'>(systemTheme);
+  // const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  // const [theme, setTheme] = useState<'light' | 'dark'>(systemTheme);
 
   // Listen for changes to the prefers-color-scheme media query
-  const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    const handleChange = (e: MediaQueryListEvent) => {
-      setTheme(e.matches ? 'dark' : 'light');
-    };
-    
-  useEffect(() => {
-    // document.documentElement.dataset.theme = lightTheme;
+  // const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  //   const handleChange = (e: MediaQueryListEvent) => {
+  //     setTheme(e.matches ? 'dark' : 'light');
+  //   };
 
-    // Check if the media query matches
-    mediaQuery.addEventListener('change', handleChange);
+  // useEffect(() => {
+  //   // document.documentElement.dataset.theme = lightTheme;
 
-    // Return a cleanup function to remove the listener
-    return () => mediaQuery.removeEventListener('change', handleChange);
-  })
-  
-  useEffect(() => {    
-    if (theme === 'dark') {
-      document.documentElement.dataset.theme = settings.darkTheme;
-    } else {
-      document.documentElement.dataset.theme = settings.lightTheme;
-    }
-  }, [theme]);
+  //   // Check if the media query matches
+  //   mediaQuery.addEventListener('change', handleChange);
 
-  const handleToggleTheme = () => {
-    if (theme === 'dark') {
-      setTheme('light');
-    } else {
-      setTheme('dark');
-    }
-  }
+  //   // Return a cleanup function to remove the listener
+  //   return () => mediaQuery.removeEventListener('change', handleChange);
+  // })
 
-  const [selectedLanguage, setSelectedLanguage] = useState<string>(localStorage.getItem('selectedLanguage') ?? '');
+  // useEffect(() => {
+  //   if (theme === 'dark') {
+  //     document.documentElement.dataset.theme = settings.darkTheme;
+  //   } else {
+  //     document.documentElement.dataset.theme = settings.lightTheme;
+  //   }
+  // }, [theme]);
+
+  // const handleToggleTheme = () => {
+  //   if (theme === 'dark') {
+  //     setTheme('light');
+  //   } else {
+  //     setTheme('dark');
+  //   }
+  // }
+
+  // const [selectedLanguage, setSelectedLanguage] = useState<string>(localStorage.getItem('selectedLanguage') ?? '');
   const [useUserPreferedLanguage, setUseUserPreferedLanguage] = useState<boolean>(false);
 
   const awailableLanguages = ['en', 'de', 'hu'];
 
-  useEffect(() => {
+  // useEffect(() => {
     // Get the user's language
-    const getUserLang = window.navigator.language.substring(0, 2);
+    // const getUserLang = window.navigator.language.substring(0, 2);
 
     // Check if user already selected a language
-    if (!useUserPreferedLanguage || !localStorage.getItem('selectedLanguage')) {
+    // if (!useUserPreferedLanguage || !localStorage.getItem('selectedLanguage')) {
 
-      // Check if the user's language is available
-      if (awailableLanguages.includes(getUserLang)) {
+      // // Check if the user's language is available
+      // if (awailableLanguages.includes(getUserLang)) {
 
-        // Set the user's language as the selected language
-        setSelectedLanguage(getUserLang);
-      } else {
+      //   // Set the user's language as the selected language
+      //   setSelectedLanguage(getUserLang);
+      // } else {
 
-        // Set the default language as the selected language
-        setSelectedLanguage('en');
-      }
+      //   // Set the default language as the selected language
+      //   setSelectedLanguage('en');
+      // }
 
-    } else {
-      localStorage.setItem('selectedLanguage', selectedLanguage);
-    }
+    // } else {
+    //   localStorage.setItem('selectedLanguage', selectedLanguage);
+    // }
 
-  }, [selectedLanguage]);
+  // }, [selectedLanguage]);
 
-  const showNextLanguage = () => {
-    const currentIndex = awailableLanguages.indexOf(selectedLanguage);
-    const nextIndex = (currentIndex + 1) % awailableLanguages.length;
-    setSelectedLanguage(awailableLanguages[nextIndex]);
-    localStorage.setItem('selectedLanguage', selectedLanguage);
-  };
+  // const showNextLanguage = () => {
+  //   const currentIndex = awailableLanguages.indexOf(selectedLanguage);
+  //   const nextIndex = (currentIndex + 1) % awailableLanguages.length;
+  //   setSelectedLanguage(awailableLanguages[nextIndex]);
+  //   localStorage.setItem('selectedLanguage', selectedLanguage);
+  // };
 
-  const getTextInSelectedLanguage = (obj: any, id: string) => {
-    switch (selectedLanguage) {
-      case 'en':
-        return obj[`${id}En`];
-      case 'de':
-        return obj[`${id}De`];
-      case 'hu':
-        return obj[`${id}Hu`];
-      default:
-        return obj[`${id}De`];
-    }
-  };
+  // const getTextInSelectedLanguage = (obj: any, id: string) => {
+  //   switch (selectedLanguage) {
+  //     case 'en':
+  //       return obj[`${id}En`];
+  //     case 'de':
+  //       return obj[`${id}De`];
+  //     case 'hu':
+  //       return obj[`${id}Hu`];
+  //     default:
+  //       return obj[`${id}De`];
+  //   }
+  // };
 
-  const handleSelectLanguage = () => {
-    setUseUserPreferedLanguage(true);
-    showNextLanguage();
-  };
+  // const handleSelectLanguage = () => {
+  //   setUseUserPreferedLanguage(true);
+  //   showNextLanguage();
+  // };
 
   // React.useEffect(() => {
   //   setTimeout(() => {
@@ -142,7 +142,7 @@ const Nav: React.FC<INav> = ({ data: [menu, settingsArr] }) => {
                       key={index}
                       index={index}
                       selected={selectedMenuItem === item.link}
-                      title={getTextInSelectedLanguage(item, 'title')}
+                      title={item.titleEn}
                       link={item.link}
                       handleClick={handleClick}
                       isDropdown={true}
@@ -159,7 +159,7 @@ const Nav: React.FC<INav> = ({ data: [menu, settingsArr] }) => {
               href="/"
               className="btn btn-ghost normal-case text-xl"
               onClick={() => scrollToId('hero')}
-              // onClick={() => handleClick(-1)}
+            // onClick={() => handleClick(-1)}
             >{settings.homepageTitle}</Link>
           </div>
         </div>
@@ -173,7 +173,7 @@ const Nav: React.FC<INav> = ({ data: [menu, settingsArr] }) => {
                     key={index}
                     index={index}
                     selected={selectedMenuItem === item.link}
-                    title={getTextInSelectedLanguage(item, 'title')}
+                    title={item.titleEn}
                     link={item.link}
                     handleClick={handleClick}
                     isDropdown={false}
@@ -187,29 +187,29 @@ const Nav: React.FC<INav> = ({ data: [menu, settingsArr] }) => {
         <div className="navbar-end">
 
           {/* Email button */}
-          <div className={`md:tooltip md:tooltip-sm mx-1 ${isTooltipOpen ? `md:tooltip-open` : ''} md:tooltip-bottom`} data-tip={settings.emailTooltipText}>
+          <div className={`md:tooltip md:tooltip-sm mx-1 ${isTooltipOpen ? `md:tooltip-open` : ''} md:tooltip-bottom`} data-tip={settings.emailTooltipTextEn}>
             <a href={`mailto:${settings.email}`} className="btn lg:btn-md btn-sm btn-secondary text-white">@</a>
           </div>
 
           {/* Theme toggler button */}
-          <div className={`md:tooltip md:tooltip-sm mx-1 ${isTooltipOpen ? `md:tooltip-open` : ''} md:tooltip-bottom`} data-tip={settings.themeButtonTooltipText}>
+          {/* <div className={`md:tooltip md:tooltip-sm mx-1 ${isTooltipOpen ? `md:tooltip-open` : ''} md:tooltip-bottom`} data-tip={settings.themeButtonTooltipTextEn}>
             <button className="btn lg:btn-md btn-sm btn-secondary text-white px-[5px] lg:px-[10px]" onClick={handleToggleTheme}>
               {theme === 'light' ? <MoonIco /> : <SunIco />}
             </button>
-          </div>
+          </div> */}
 
           {/* Language button */}
-          <div className={`md:tooltip md:tooltip-sm mx-1 ${isTooltipOpen ? `md:tooltip-open` : ''} md:tooltip-bottom`} data-tip={settings.themeButtonTooltipText}>
+          {/* <div className={`md:tooltip md:tooltip-sm mx-1 ${isTooltipOpen ? `md:tooltip-open` : ''} md:tooltip-bottom`} data-tip={settings.themeButtonTooltipTextEn}>
             <button className="btn lg:btn-md btn-sm btn-secondary text-white px-[5px] lg:px-[10px]" onClick={() => handleSelectLanguage()}>
               {selectedLanguage === 'de' && <FlagDe />}
               {selectedLanguage === 'en' && <FlagEn />}
               {selectedLanguage === 'hu' && <FlagHu />}
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Nav;
