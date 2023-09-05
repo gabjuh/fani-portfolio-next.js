c5112b94dc2a8ebb8da013500ebe087727fd703d
gabjuh <jjjjgabesz@gmail.com>
2023-09-05 06:13:46 +0200

    set next config api url

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        // hostname: 'api-franciskahajdu.web4musicians.eu',
-        hostname: process.env.NEXT_PUBLIC_BACKEND_API,
+        hostname: 'api-franciskahajdu.web4musicians.eu',
         port: '',
         pathname: '/**',
       },
