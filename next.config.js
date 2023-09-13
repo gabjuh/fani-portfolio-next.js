95457ef9ee217415e03ee62f9e863b6fe2519914
gabjuh <jjjjgabesz@gmail.com>
2023-09-13 06:09:08 +0200

    add discography domain as acceptable domain

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'discography-franciskahajdu.web4musicians.eu',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
