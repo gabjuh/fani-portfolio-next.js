ff2919ec34292168b2e80ade1a2d5089cb46bbff
gabjuh <jjjjgabesz@gmail.com>
2023-09-14 06:06:17 +0200

    change media query from md to lg

--- app/components/ImageAndText.tsx
+++ app/components/ImageAndText.tsx
@@ -19,21 +19,21 @@ const ImageAndText: React.FC<IImageAndText> = ({
 
   return (
     <>
-      <div className={`flex ${imageLeft ? 'flex-col' : 'flex-col-reverse'} md:flex-row lg:my-16 mb-4 pt-24`} id="about-me">
+      <div className={`flex ${imageLeft ? 'flex-col' : 'flex-col-reverse'} lg:flex-row lg:my-16 mb-4 pt-24`} id="about-me">
         {imageLeft && <ImageForText fileName={fileName} driveId={driveId || ''} alt={alt || 'image'} classNameForImg={classNameForImg} />}
-        <div className={`w-full md:w-4/6 flex flex-col ${!imageLeft ? 'items-end' : ''} justify-center md:ml-10 mr-10`}>
+        <div className={`w-full lg:w-4/6 flex flex-col ${!imageLeft ? 'items-end' : ''} justify-center lg:ml-2 mr-10`}>
           <div 
-            className={`${imageLeft ? textAlign === 'justify' ? 'md:text-justify' : 'md:text-left' : textAlign === 'justify' ? 'md:text-justify' : 'md:text-right'} md:ml-10 lg:ml-0 lg:mr-0 text-center leading-8 px-8 md:p-0`}
+            className={`${imageLeft ? textAlign === 'justify' ? 'lg:text-justify' : 'lg:text-left' : textAlign === 'justify' ? 'lg:text-justify' : 'lg:text-right'} lg:mx-0 mx-4 text-center leading-8 px-8 lg:p-0`}
           >
             {/* {title && <Title title={title} className="md:text-right uppercase text-4xl" />} */}
-            {title && <h2 className={`${imageLeft ? 'md:text-left' : 'md:text-right'} uppercase text-4xl mb-3`}>{title}</h2>}
+            {title && <h2 className={`${imageLeft ? 'lg:text-left' : 'lg:text-right'} uppercase text-4xl mb-3`}>{title}</h2>}
             <ReactMarkdown
               remarkPlugins={[remarkGfm]}
               children={text}
             />
           </div>
           {buttonText &&
-            <p className={`flex-none text-center ${imageLeft ? textAlign === 'justify' ? 'md:text-justify' : 'md:text-left' : textAlign === 'justify' ? 'md:text-justify' : 'md:text-right'} mt-5 w-[100%]`}>
+            <p className={`flex-none text-center ${imageLeft ? textAlign === 'justify' ? 'lg:text-justify' : 'lg:text-left' : textAlign === 'justify' ? 'lg:text-justify' : 'lg:text-right'} mt-5 w-[100%]`}>
               <Link
                 className="btn btn-sm btn-secondary text-white"
                 href="/cv"
--- app/components/ImageForText.tsx
+++ app/components/ImageForText.tsx
@@ -9,7 +9,7 @@ const ImageForText: React.FC<IImageForText> = ({
 }) => {
   return (
     <>
-      <div className="w-full md:w-1/2">
+      <div className="w-full lg:w-1/2">
         <Image 
           src={`https://${process.env.NEXT_PUBLIC_BACKEND_API}/img/${fileName}`}
           // placeholder="blur"
@@ -20,7 +20,7 @@ const ImageForText: React.FC<IImageForText> = ({
             width: '380px',
             height: 'auto',
           }}
-          className={`${classNameForImg} mx-auto mb-6 md:mb-0`} 
+          className={`${classNameForImg} mx-auto mb-6 lg:mb-0`} 
         />
       </div>
     </>
